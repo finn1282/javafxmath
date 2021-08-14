e146e6559d82ff3784901b77434c91b02405d5a6
finn1282 <themistterrain@example.com>
2021-08-15 00:32:35 +0800

    fixed gitignore

--- src/test/main.java
+++ src/test/main.java
@@ -1,8 +1,6 @@
 package test;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
